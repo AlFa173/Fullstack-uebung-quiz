cf2745c952d65cc70dfd4d0c3a2e8de79fddbf61
a.faber <a.faber@accenture.com>
2026-09-16 17:01:17 +0200

    script updated

--- script.js
+++ script.js
@@ -1,1 +1,69 @@
-console.log("Hello World");
+// let dasIstEinBoolean = true;
+// let dasIstEineZahl = 5;
+// let dasIstEinString = "Hallo Welt";
+// let dasIstKeinWert;
+// console.log(dasIstKeinWert, dasIstEinBoolean, dasIstEineZahl, dasIstEinString);
+
+let zahl1 = 5;
+let zahl2 = 10;
+
+// console.log(zahl1 + zahl2);
+// console.log(zahl1 - zahl2);
+// console.log(zahl1 * zahl2);
+// console.log(zahl1 / zahl2);
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// while (zahl1 < 20)
+//     {
+//     console.log(zahl1);
+//     zahl1++;
+// }
+
+// if (zahl1 < zahl2) {
+//     console.log("Zahl1 ist kleiner als Zahl2");
+// }
+// else if (zahl1 > zahl2) {
+//     console.log("Zahl1 ist größer als Zahl2");
+// }   
+
+// let firstName = "Alexander";
+// let lastName = "Faber";
+
+// let fullName = firstName + " " + lastName;
+
+// console.log("Alpha" < "Beta");
+
+
+// function addNumbers(zahl1, zahl2)
+// {
+//     let ergebnis = zahl1 + zahl2;
+
+//     return ergebnis;
+// }
+
+// let ergebnis = addNumbers(zahl1, zahl2);
+// console.log(ergebnis);
+
+
+// function functionName()
+// {
+//     console.log("Hello");
+//     console.log("World");
+//     console.log("!");
+// }
+
+// for (let i = 0; i <= 2; i++) 
+// {
+//     console.log(functionName());
+// }
+
+function multiplyNumbers(zahl1, zahl2)
+{
+    let ergebnis = zahl1 * zahl2;
+    return ergebnis;
+}   
+
+console.log(multiplyNumbers(6, 8));
